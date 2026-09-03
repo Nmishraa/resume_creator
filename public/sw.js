7d93edfbe6a7a8b8f4472007fc8e97fbe2f9bf9d
Nmishraa <112948128+Nmishraa@users.noreply.github.com>
2026-09-03 04:53:43 -0500

    fix: Add automatic SW unregistration and self-destructing sw.js to clear client browser cache

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
